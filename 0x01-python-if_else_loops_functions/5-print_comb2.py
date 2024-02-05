5f0a5ec796443121fd612cde8f1fd4f0c5781ccf
IanoNjuguna <inc3099@gmail.com>
2024-02-05 16:06:20 +0300

    5-print_comb2.py: use two format strings

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,9 +2,9 @@
 
 for number in range(0, 100):
     if number < 10:
-        print(0, end="")
-
-    print("{}".format(number), end="")
+        print("0{}".format(number), end="")
+    else:
+        print("{}".format(number), end="")
 
     if number != 99:
         print(", ", end="")
