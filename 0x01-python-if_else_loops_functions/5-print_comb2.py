31ae8b8564a5091f1b85dbcf1393c01a4cb76aa2
IanoNjuguna <inc3099@gmail.com>
2024-02-05 16:01:05 +0300

    5-print_comb2.py: use format strings instead of f-strings

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -4,7 +4,7 @@ for number in range(0, 100):
     if number < 10:
         print(0, end="")
 
-    print(f"{number:d}", end="")
+    print("{}".format(number), end="")
 
     if number != 99:
         print(", ", end="")
