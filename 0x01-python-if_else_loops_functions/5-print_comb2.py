afc68e5e2d6270cdc71afea7c4b7d8a3485faa83
IanoNjuguna <inc3099@gmail.com>
2024-02-06 14:20:41 +0300

    5-print_comb2.py: print numbers from 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 
 for number in range(0, 100):
-    if number < 10:
-        print("0{}".format(number), end="")
-    else:
-        print("{}".format(number), end="")
-
     if number != 99:
-        print(", ", end="")
+        print("{}".format(number), end=", ")
     else:
-        print("\n")
+        print("{}".format(number))
