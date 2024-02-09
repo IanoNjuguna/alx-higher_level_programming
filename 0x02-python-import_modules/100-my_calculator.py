fae3ca3be0a554d2c1aeba9bdfa80da3f17f73d5
IanoNjuguna <inc3099@gmail.com>
2024-02-09 13:26:20 +0300

    100-my_calculator:  imports all functions from the file calculator_1.py and handles basic operations.
    * Usage: ./100-my_calculator.py a operator b
    	* If the number of arguments is not 3, your program has to:
    		* print Usage: ./100-my_calculator.py <a> <operator> <b> followed with a new line
    		* exit with the value 1
    	* Operator can be:
    		+ for addition
    		- for subtraction
    		* for multiplication
    		/ for division
    	* If the operator is not one of the above:
    		* print Unknown operator. Available operators: +, -, * and / followed with a new line
    		* exit with the value 1
    	* Can cast a and b into integers by using int() (you can assume that all arguments will be castable into integers)
    	* Result should be printed like this: <a> <operator> <b> = <result>, followed by a new line
    * Not allowed to use * for importing or __import__
    * Code should not be executed when imported

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv, exit
+
+    len_argv = len(argv)
+    argc = len_argv - 1
+    operators = {"+" : add, "-" : sub, "*" : mul, "/" : div}
+    a = int(argv[1])
+    b = int(argv[3])
+
+    if argc != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    elif argv[2] not in list(operators.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    else:
+        print("{} {} {} = {}".format(a, argv[2], b, operators[argv[2]](a, b)))
