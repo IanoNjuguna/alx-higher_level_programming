d696e84bf522e2d5560b5fb12f12e9efa9477290
IanoNjuguna <inc3099@gmail.com>
2024-02-12 16:08:39 +0300

    3-print_reversed_list_integer.py: introduce error checking

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
+    if not isinstance(my_list, list):
+        return 1
+    else:
+        my_list.reverse()
 
-    my_list.reverse()
-
-    for item in my_list:
-        print("{:d}".format(item))
+        for item in my_list:
+            print("{:d}".format(item))
