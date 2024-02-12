b3a00dd5ed941935894eaa8578e229be61551248
IanoNjuguna <inc3099@gmail.com>
2024-02-12 15:18:19 +0300

    1-element_at.py: retireves an element in a list like in C

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -5,9 +5,9 @@ def element_at(my_list, idx):
     list_index = len(my_list) - 1
     list_length = len(my_list)
 
-    if list_length < 0:
+    if idx < 0:
         return None
-    elif list_length > list_index:
+    elif idx > list_index:
         return None
     else:
         return my_list[idx]
