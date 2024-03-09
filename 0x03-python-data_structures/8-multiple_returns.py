4420eac046babb6fe9bf440baf6eafcebaee220c
IanoNjuguna <ichege438@gmail.com>
2024-03-09 19:57:26 +0300

    8-multiple_returns.py: returns a tuple with the length of a string and its first character.
    * If the sentence is empty, the first character is equal to None

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    len_sentence = len(sentence)
+    
+    if len_sentence == 0:
+        first_char = None
+    else:
+        first_char = sentence[0]
+
+    tuple_new = len_sentence, first_char
+
+    return tuple_new
