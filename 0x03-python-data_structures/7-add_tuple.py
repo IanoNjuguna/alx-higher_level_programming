e6cef63149c97640d2ac5ff8ab1452f27d809aea
IanoNjuguna <ichege438@gmail.com>
2024-03-09 17:30:11 +0300

    7-add_tuple.py: handle branching for each tuple individually

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,16 +5,16 @@ def add_tuple(tuple_a=(), tuple_b=()):
     len_b = len(tuple_b)
 
     if len_a < 2:
-        tuple_a = tuple_a[0], 0
-        
-    elif len_a == 0:
-        tuple_a = 0, 0
+        if len_a == 0:
+            tuple_a = 0, 0
+        else:
+            tuple_a = tuple_a[0], 0
 
-    elif len_b < 2:
-        tuple_b = tuple_b[0], 0
-    
-    elif len_b == 0:
-        tuple_b = 0, 0
+    if len_b < 2:
+        if len_b == 0:
+            tuple_b = 0, 0
+        else:
+            tuple_b = tuple_b[0], 0
 
     tuple_new = [(tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1])]
 
