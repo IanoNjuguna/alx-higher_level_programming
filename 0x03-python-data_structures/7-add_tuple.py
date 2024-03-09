b14fac9278b773ac956821448bdfb61660984fe2
IanoNjuguna <ichege438@gmail.com>
2024-03-09 17:33:36 +0300

    7-add_tuple.py: fix how the code handles tuples that have a length greater than 2
    * If a tuple is bigger than 2, use only the first 2 integers

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -10,11 +10,17 @@ def add_tuple(tuple_a=(), tuple_b=()):
         else:
             tuple_a = tuple_a[0], 0
 
+    if len_a > 2:
+        tuple_a = tuple_a[0], tuple_a[1]
+
     if len_b < 2:
         if len_b == 0:
             tuple_b = 0, 0
         else:
             tuple_b = tuple_b[0], 0
+
+    if len_b > 2:
+        tuple_b = tuple_b[0], tuple_b[1]
 
     tuple_new = [(tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1])]
 
