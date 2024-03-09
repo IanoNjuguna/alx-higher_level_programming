8ec3e98ccf9417a27feede5786058b87ab278c05
IanoNjuguna <ichege438@gmail.com>
2024-03-09 17:25:18 +0300

    7-add_tuple.py: If a tuple is smaller than 2, use the value 0 for each missing integer

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a = tuple_a + (0, 0)
-    tuple_b = tuple_b + (0, 0)
+    len_a = len(tuple_a)
+    len_b = len(tuple_b)
+
+    if len_a < 2:
+        tuple_a = tuple_a[0], 0
+        
+    elif len_a == 0:
+        tuple_a = 0, 0
+
+    elif len_b < 2:
+        tuple_b = tuple_b[0], 0
+    
+    elif len_b == 0:
+        tuple_b = 0, 0
 
     tuple_new = [(tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1])]
 
